363d32cea820a5eb2c5042dfc46ebd4202a7cb1b
Nikolay Sidorenko <gsidnick@gmail.com>
2024-03-07 18:09:48 +0300

    chore: change swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
+import { parse } from 'yaml';
+import { join } from 'path';
+import { readFile } from 'fs/promises';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Home Library Service')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const filename = join(__dirname, '../doc/api.yaml');
+  const config = await readFile(filename, 'utf-8');
+  const document = parse(config);
   SwaggerModule.setup('doc', app, document);
 
   await app.listen(4000);
