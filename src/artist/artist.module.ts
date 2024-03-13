60f9d8feb3af16b7abe23c17cd226be9f6001c7c
Nikolay Sidorenko <gsidnick@gmail.com>
2024-03-13 08:44:52 +0300

    refactor: change artist interaction with database

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { ArtistController } from './artist.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ArtistController],
   providers: [ArtistService],
 })
