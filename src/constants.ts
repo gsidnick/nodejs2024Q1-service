4ddac385bb06acd7066bb46185066ce78d3d510e
Nikolay Sidorenko <gsidnick@gmail.com>
2024-03-28 20:39:29 +0300

    feat: add logger service logic

--- src/constants.ts
+++ src/constants.ts
@@ -8,6 +8,8 @@ export const DEFAULT_SECRET_KEY = 'secret123123';
 export const DEFAULT_SECRET_REFRESH_KEY = 'secret123123';
 export const DEFAULT_EXPIRE_TIME = '1h';
 export const DEFAULT_REFRESH_EXPIRE_TIME = '24h';
+export const DEFAULT_LOG_LEVEL = 0;
+export const DEFAULT_LOG_SIZE = 1;
 
 export const PORT: number = Number(process.env.PORT) || DEFAULT_PORT;
 export const SALT: number = Number(process.env.CRYPT_SALT) || DEFAULT_SALT;
@@ -19,3 +21,6 @@ export const EXPIRE_TIME =
   String(process.env.TOKEN_EXPIRE_TIME) || DEFAULT_EXPIRE_TIME;
 export const REFRESH_EXPIRE_TIME =
   String(process.env.TOKEN_REFRESH_EXPIRE_TIME) || DEFAULT_REFRESH_EXPIRE_TIME;
+export const LOG_LEVEL = Number(process.env.LOG_LEVEL) || DEFAULT_LOG_LEVEL;
+export const LOG_SIZE =
+  (Number(process.env.LOG_SIZE) || DEFAULT_LOG_SIZE) * 1024;
--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,24 +1,120 @@
-import { LoggerService as ILoggerService, Injectable } from '@nestjs/common';
+import { stat, writeFile, rename, mkdir } from 'node:fs/promises';
+import { join } from 'node:path';
+import {
+  LoggerService as ILoggerService,
+  Injectable,
+  LogLevel,
+} from '@nestjs/common';
+import { LOG_LEVEL, LOG_SIZE } from 'src/constants';
 
 @Injectable()
 export class LoggerService implements ILoggerService {
+  private readonly level: number;
+  private readonly size: number;
+  private readonly dir: string;
+
+  constructor() {
+    this.level = LOG_LEVEL;
+    this.size = LOG_SIZE;
+    this.dir = join(`./logs/`);
+    this.makeLogDir(this.dir);
+  }
+
   log(data: any) {
-    /* your implementation */
+    this.write('log', data);
   }
 
   error(data: any) {
-    /* your implementation */
+    this.write('error', data);
   }
 
   warn(data: any) {
-    /* your implementation */
+    this.write('warn', data);
   }
 
   debug?(data: any) {
-    /* your implementation */
+    this.write('debug', data);
   }
 
   verbose?(data: any) {
-    /* your implementation */
+    this.write('verbose', data);
+  }
+
+  private async write(type: LogLevel, data) {
+    const [level, color] = this.useLevelColor(type);
+
+    if (level > this.level) {
+      return;
+    }
+
+    const pid = process.pid;
+    const datetime = new Date(Date.now()).toLocaleString();
+    const message = `[Nest] ${pid}  - ${datetime}     ${data}`;
+
+    this.printToConsole(color, message);
+
+    if (type === 'error') {
+      await this.writeToFile('errors', message);
+    } else {
+      await this.writeToFile('logs', message);
+    }
+  }
+
+  private async writeToFile(
+    name: 'errors' | 'logs',
+    data: string,
+  ): Promise<void> {
+    const file = `${name}.log`;
+    const filename = join(this.dir, file);
+
+    try {
+      const stats = await stat(filename);
+
+      if (!stats.isFile || stats.size >= this.size) {
+        const archiveFilename = this.getArchiveFilename(name);
+        await rename(filename, archiveFilename);
+      }
+    } catch {
+    } finally {
+      await writeFile(filename, `${data}\n`, { flag: 'a' });
+    }
+  }
+
+  private async makeLogDir(name: string) {
+    try {
+      await mkdir(name, { recursive: true });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  private printToConsole(color: string, data: string): void {
+    console.log(`${color}${data}\x1b[0m`);
+  }
+
+  private getArchiveFilename(name: string) {
+    const datetime = new Date(Date.now())
+      .toISOString()
+      .replace(/[:\.]+/g, '-')
+      .replace(/[T]+/g, '_');
+
+    return join(`./logs/${name}_${datetime}.log`);
+  }
+
+  private useLevelColor(type: LogLevel): [number, string] {
+    switch (type) {
+      case 'error':
+        return [1, '\x1b[31m'];
+      case 'warn':
+        return [2, '\x1b[33m'];
+      case 'log':
+        return [3, '\x1b[32m'];
+      case 'verbose':
+        return [4, '\x1b[36m'];
+      case 'debug':
+        return [5, '\x1b[37m'];
+      default:
+        return [0, '\x1b[37m'];
+    }
   }
 }
